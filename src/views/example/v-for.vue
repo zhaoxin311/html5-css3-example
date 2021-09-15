daf1cde3fac747eb3572df84656f66fa119aed8a
zhaoxin0311 <1204090769@qq.com>
2021-09-15 23:03:51 +0800

    v-for 循环还有点小问题

--- src/views/example/v-for.vue
+++ src/views/example/v-for.vue
@@ -8,6 +8,13 @@
                 {{item}}
             </li>
         </ul>
+        <hr>
+        <ul>
+            <li v-for="(student ,index) in students" :key="index">
+               {{index+1}}. 姓名：{{student.name}}---年龄：{{student.age}}
+
+            </li>
+        </ul>
     </div>
   </div>
 </template>
@@ -16,8 +23,21 @@ export default {
   name: "app",
   data() {
     return {
-     items:[25,52,41,69,53,47,55,84,55]
+     items:[25,52,41,69,53,47,55,84,55],
+     students :[
+         {name:'西还会',age:11},
+         {name:'找找找',age:12},
+         {name:'赵化通',age:12},
+         {name:'赵欣欣',age:51},
+         {name:'赵小溪',age:43},
+         {name:'小花花',age:21},
+     ]
     };
   },
+  computed:{
+    // sortItems:function(){
+    //       return this.items.sort();
+    // }
+}
 };
 </script>
